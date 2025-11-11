bb0249f2edf92e8cc92025a86c9024499f64543a
shahnoza <ollaberganovashakhnoza@gmail.com>
2025-11-11 16:45:53 +0500

    switch new work

---
+++ javascript/24-dars switch case/switch.js
@@ -0,0 +1,34 @@
+// console.log('test');
+// let day = 10
+// switch (day) {
+//     case 1: console.log("monday"); break
+//     case 2: console.log("tuesday");break
+//     case 3: console.log("wednesday");break
+//     case 4: console.log("thursday");break
+//     case 5: console.log('friday');break
+//     case 6: console.log("saturday");break
+//     case 7: console.log('sunday');break
+//     default: console.log("bunday kun yoq. Adashding bratishka");
+    
+// }
+
+// let month = 13
+// switch (month) {
+//     case 1: console.log("qish");break
+//     case 2: console.log("qish");break
+//     case 3: console.log("bahor");break
+//     case 4: console.log("bahor");break
+//     case 5: console.log("bahor");break
+//     case 6: console.log("yoz");break
+//     case 7: console.log("yoz");break
+//     case 8: console.log("yoz");break
+//     case 9: console.log("kuz");break
+//     case 10: console.log("kuz");break
+//     case 11: console.log("kuz");break
+//     case 12: console.log("qish");break
+
+
+//     default: console.log("bunday oy yoq");
+    
+// }
+
