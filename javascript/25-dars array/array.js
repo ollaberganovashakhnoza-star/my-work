af3d5ddc4e0ec209e9d5bfdf2c7ab94c77a52412
shahnoza <ollaberganovashakhnoza@gmail.com>
2025-11-13 16:31:22 +0500

    js array

---
+++ javascript/25-dars array/array.js
@@ -0,0 +1,19 @@
+// let a = 15
+// let b = 20
+// console.log(a+b);
+
+// let carnames = ["BMW" , "MERSDES BENZ" , "PORSCHE" ,"AUDI" , "BWD"]
+// console.log(carnames);
+// console.log(carnames. length);
+// console.log(carnames[4]);
+
+// carnames [2] = "matiz";
+
+//  console.log(carnames);
+                                                                                                        
+// carnames.push("lamborgini")
+// carnames.unshift("machine")
+// carnames.pop();
+// carnames.shift();
+
+
---
+++ javascript/25-dars array/task.js
@@ -0,0 +1,14 @@
+// let numbers = [25, 55, 15, 17, 3, 66, 34, 45, 33]
+
+// console.log(numbers [0] + numbers [1] + numbers [2] + numbers [3] + numbers [4] +  numbers [5] +  numbers [6] +  numbers [7] +  numbers [8]);
+
+let numbers = [75, 55]
+let a = numbers [0]
+let b = numbers [1]
+if(a < b){
+    console.log(b);
+    
+}else if(b < a){
+    console.log(a);
+    
+}
