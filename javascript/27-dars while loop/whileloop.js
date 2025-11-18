31919941de464dbca947a11ac08c2bffabd870fc
shahnoza <ollaberganovashakhnoza@gmail.com>
2025-11-18 16:31:46 +0500

    whileloop

---
+++ javascript/27-dars while loop/whileloop.js
@@ -0,0 +1,43 @@
+// let a = 3;  
+// let b = 10; 
+
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     sum += i;
+// }
+
+// console.log("Yig'indi =", sum);
+
+// let a = 12
+// let b = 18
+
+// let sum = 0
+
+// for (let i = a; i <= b; i++){
+//     sum += i
+// }
+
+// console.log("yigindi =", sum);
+
+
+// let a = 23
+// let b = 25
+
+// let sum = 1
+// for ( i = a; i <= b; i++){
+//     sum *= i
+    
+// }
+// console.log("yigindi =", sum);
+
+let names = ["shaxruz", "guli", "anaxon", "shahnoza", "zulayho",
+    "dilyor", "shahzod", "alixon", "jaxongir"
+]
+
+for (let i=0; i < names.length; i++){
+    if (names[i] != "guli" && "shahnoza" && "anaxon" && "zulayho") {
+        console.log(names[i] + "bek");
+        
+    }
+} 
