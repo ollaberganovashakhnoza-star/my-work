8d9314e529d73082066719d0ede023ddab6996b5
shahnoza <ollaberganovashakhnoza@gmail.com>
2025-11-20 16:33:32 +0500

    repeat

--- javascript/27-dars while loop/whileloop.js
+++ javascript/27-dars while loop/whileloop.js
@@ -31,13 +31,51 @@
 // }
 // console.log("yigindi =", sum);
 
-let names = ["shaxruz", "guli", "anaxon", "shahnoza", "zulayho",
-    "dilyor", "shahzod", "alixon", "jaxongir"
-]
+// let names = ["shaxruz", "guli", "anaxon", "shahnoza", "zulayho",
+//     "dilyor", "shahzod", "alixon", "jaxongir"
 
-for (let i=0; i < names.length; i++){
-    if (names[i] != "guli" && "shahnoza" && "anaxon" && "zulayho") {
-        console.log(names[i] + "bek");
-        
-    }
-} 
+
+
+
+// for (let i=0; i < names.length; i++){
+//  if (
+//     names[i] !== "guli" &&
+//     names[i] !== "anaxon" &&
+//     names[i] !== "shahnoza" &&
+//     names[i] !== "zulayho"
+// ) {
+//     console.log(names[i] + " bek");
+// }
+
+// }
+
+
+
+// let a = 2
+// let n = 5
+
+// let k =1
+
+// for (let i = 1; i <= n; i++) {
+//    k= k*a 
+// }
+
+//  console.log(k);
+
+
+
+//  let a = 2
+//  let b = 10
+//  let s = 0
+
+
+//  for ( i = a; i <= b; i++){
+//      s = s + i*i
+    
+//  }
+
+//  console.log(s);
+ 
+
+
+
