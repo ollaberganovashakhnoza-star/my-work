a34b8015a02d3ce51d35b135665d3ca8078d7c37
shahnoza <ollaberganovashakhnoza@gmail.com>
2025-12-04 16:23:02 +0500

    objects

---
+++ javascript/39-dars objects/objects.js
@@ -0,0 +1,18 @@
+let query = prompt("nima boyicha qidirasiz ...?")
+const user = {
+    name: "shahnoza",
+    surname: "ollaberganova",
+    age: 14,
+    address: "o'rjon elot",
+    phone: "+998 95 659 35 31",
+    school: 23,
+    grade: "8A",
+    password: "1234"
+}
+
+
+
+if (user[query] == undefined) {
+   alert("bunday narsa yo'q");
+    
+}
